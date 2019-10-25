bf85884f98f7105bb35e430d7029c1be3f870f10
Rob Thornton <rob.thornton@shopify.com>
2019-10-25 09:57:36 -0400

    rename file extensions back to js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Article from './Components/Articles';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Article />
     </div>
   );
 }
