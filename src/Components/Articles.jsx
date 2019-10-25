a634c1b4541e81fa35d716a86a46592203d926ac
Rob Thornton <rob.thornton@shopify.com>
2019-10-25 11:01:37 -0400

    fetch array of articles

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,7 +1,20 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
+import Axios from 'axios';
 
 function Articles() {
   const [data, setData] = useState({hits: []});
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await Axios(
+        'https://hn.algolia.com/api/v1/search?query=redux'
+      );
+
+      setData(result.data);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <ul>
