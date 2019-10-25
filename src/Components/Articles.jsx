2c5e1b42f32204d9f033ccebf217b13bb89d81c5
Rob Thornton <rob.thornton@shopify.com>
2019-10-25 11:56:55 -0400

    implement full component with custom fetch hook

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,29 +1,70 @@
 import React, {useEffect, useState} from 'react';
 import Axios from 'axios';
 
-function Articles() {
-  const [data, setData] = useState({hits: []});
+const useDataqApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   useEffect(() => {
-    async function fetchData() {
-      const result = await Axios(
-        'https://hn.algolia.com/api/v1/search?query=redux'
-      );
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
 
-      setData(result.data);
-    }
+      try {
+        const result = await Axios(url);
+
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+
+      setIsLoading(false);
+    };
 
     fetchData();
-  }, []);
+  }, [url]);
+
+  return [{data, isLoading, isError}, setUrl];
+};
+
+function Articles() {
+  const [query, setQuery] = useState('redux');
+  const [{data, isLoading, isError}, doFetch] = useDataqApi(
+    'https://hn.algolia.com/api/v1/search?query=redux',
+    {hits: []}
+  );
 
   return (
-    <ul>
-      {data.hits.map((item) => (
-        <li key={item.objectID}>
-          <a href={item.url}>{item.title}</a>
-        </li>
-      ))}
-    </ul>
+    <>
+      <form
+        onSubmit={(event) => {
+          doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`);
+          event.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={query}
+          onChange={(event) => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isError && <div>Something went wrong...</div>}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {data.hits.map((item) => (
+            <li key={item.objectID}>
+              <a href={item.url}>{item.title}</a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
