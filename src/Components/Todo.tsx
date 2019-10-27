8be1d0b9b8217924dda6f28c453a22d4eb47d7cd
Rob Thornton <rob.thornton@shopify.com>
2019-10-26 20:25:22 -0400

    props should be optional; use hook for input value

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 interface WithTodoProps {
-  task: string;
-  completed: boolean;
+  task?: string;
+  completed?: boolean;
 }
 
 function Todo({task, completed}: WithTodoProps): React.ReactElement {
+  const [text, setText] = useState(task);
+
   return (
     <div className="todo">
       <input
         type="text"
         className={completed ? 'completed' : undefined}
-        value={task}
+        value={text}
+        onChange={(event) => setText(event.target.value)}
       />
     </div>
   );
