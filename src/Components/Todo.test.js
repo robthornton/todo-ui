32c2773e0b52dbc191ffa5d981d300f0aa4eac3f
Rob Thornton <rob.thornton@shopify.com>
2019-10-26 20:25:42 -0400

    add more tests

--- src/Components/Todo.test.js
+++ src/Components/Todo.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {act} from 'react-dom/test-utils';
 import Todo from './Todo';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Todo />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders task text', () => {
+  const div = document.createElement('div');
+  act(() => {
+    ReactDOM.render(<Todo task={'Example task'} />, div);
+  });
+  const input = div.querySelector('input');
+  expect(input.value).toBe('Example task');
+  expect(input.classList.contains('completed')).toBe(false);
+});
+
+it('renders task completed', () => {
+  const div = document.createElement('div');
+  act(() => {
+    ReactDOM.render(<Todo completed />, div);
+  });
+  const input = div.querySelector('input');
+  expect(input.classList.contains('completed')).toBe(true);
 });
