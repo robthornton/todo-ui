4eedaa87a8a3aa8f3d4fee5085fc4d6086cc5761
Rob Thornton <rob.thornton@shopify.com>
2019-11-07 16:46:07 -0500

    add title component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import TodoList from './Components/TodoList';
 import './App.css';
+import Title from './Components/Title';
 
 function App() {
   return (
     <div className="App">
+      <Title text="To-do" />
       <TodoList
         todos={[
           {id: 0, task: 'Example text', completed: false},
