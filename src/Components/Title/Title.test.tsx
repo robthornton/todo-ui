482893d83477979c289ed1afc750b40a00f4f0eb
Rob Thornton <rob.thornton@shopify.com>
2019-12-09 15:55:17 -0500

    import for title should be explicit

--- src/Components/Title/Title.test.tsx
+++ src/Components/Title/Title.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, shallow} from 'enzyme';
 
-import Title from './Title';
+import {Title} from './Title';
 
 describe('title component', () => {
   it('should render without error', () => {
