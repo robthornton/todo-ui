3af8007bbcd0afa639bab2b69ae54d76eb61ebb9
Rob Thornton <rob.thornton@shopify.com>
2019-12-10 17:21:15 -0500

    use explicit import for list

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 import {CreateTodoInput} from './CreateTodoInput/CreateTodoInput';
 import {Title} from './Title/Title';
 import Todo from './TodoList/TodoItem/TodoItem';
-import TodoList from './TodoList/TodoList';
+import {TodoList} from './TodoList/TodoList';
 
 export {CreateTodoInput, Title, Todo, TodoList};
