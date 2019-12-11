7fb3aa4779d169823f38dc5cdad427de5f936d9a
Rob Thornton <rob.thornton@shopify.com>
2019-12-11 16:55:12 -0500

    implement button

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import {Button} from './Button';
-import {render} from 'enzyme';
+import {render, shallow} from 'enzyme';
 
 describe('Button component', () => {
   it('renders without error', () => {
     const wrapper = render(<Button />);
     expect(wrapper.length).toBe(1);
+  });
+
+  it('renders text', () => {
+    const wrapper = render(<Button label="Label" />);
+    expect(wrapper.text()).toBe('Label');
+  });
+
+  it('calls onClick function on click', () => {
+    const fn = jest.fn();
+    const wrapper = shallow(<Button label="Label" onClick={fn} />);
+    wrapper.simulate('click');
+    expect(fn).toHaveBeenCalledTimes(1);
   });
 });
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export function Button(): React.ReactElement {
-  return <div />;
+type ButtonProps = {
+  label?: string;
+  onClick?: () => any;
+};
+
+export function Button({label, onClick}: ButtonProps): React.ReactElement {
+  return (
+    <button type="button" onClick={onClick}>
+      {label}
+    </button>
+  );
 }
