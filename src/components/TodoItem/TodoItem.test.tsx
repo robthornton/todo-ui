827f4a3d21c53c8f1845472928a5be950d9d0727
Rob Thornton <rob.thornton@shopify.com>
2019-12-11 16:55:50 -0500

    do not test for button behaviour

--- src/components/TodoItem/TodoItem.test.tsx
+++ src/components/TodoItem/TodoItem.test.tsx
@@ -18,32 +18,4 @@ describe('Todo component', () => {
     const input = wrapper.find('div');
     expect(input.text()).toBe('Example');
   });
-
-  it('should render without error delete button is clicked', () => {
-    const wrapper = mount(
-      <TodoItem todo={todo} onComplete={() => {}} onDelete={() => {}} />
-    );
-    const completeButton = wrapper.find('button.delete');
-    expect(completeButton.simulate('click')).toBeTruthy();
-  });
-
-  it('should use callback when delete button is clicked', () => {
-    const deleteFunction = jest.fn();
-    const wrapper = mount(
-      <TodoItem todo={todo} onComplete={() => {}} onDelete={deleteFunction} />
-    );
-    const completeButton = wrapper.find('button.delete');
-    completeButton.simulate('click');
-    expect(deleteFunction).toHaveBeenCalled();
-  });
-
-  it('should use callback when complete button is clicked', () => {
-    const callback = jest.fn();
-    const wrapper = mount(
-      <TodoItem todo={todo} onComplete={callback} onDelete={() => {}} />
-    );
-    const completeButton = wrapper.find('button.complete');
-    completeButton.simulate('click');
-    expect(callback).toHaveBeenCalled();
-  });
 });
